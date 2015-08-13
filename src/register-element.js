8f75eeea1cf3a13688395ae3f52cdc8d976da33d
Nathan Sobo <nathan@github.com>
2015-08-12 20:57:11 -0600

    :art:

--- src/register-element.js
+++ src/register-element.js
@@ -36,7 +36,6 @@ export default function registerElement (elementName, elementSpec) {
       throw new Error(`Already registered element ${elementName}. Call .unregister() on the previously-registered element constructor before registering another element with the same name.`)
     }
     Object.setPrototypeOf(elementConstructor.prototype, elementPrototype)
-    return elementConstructor
   } else {
     elementConstructor = document.registerElement(elementName, {prototype: Object.create(elementPrototype)})
     elementConstructor.unregister = () => {
@@ -47,6 +46,7 @@ export default function registerElement (elementName, elementSpec) {
       }
     }
     elementConstructors[elementName] = elementConstructor
-    return elementConstructor
   }
+  
+  return elementConstructor
 }
