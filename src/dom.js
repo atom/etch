6d552abfa8d5144472584d4fd63ebaac25954023
Nathan Sobo <nathan@github.com>
2015-08-17 12:10:15 -0600

    Add reference tracking for child components

--- src/dom.js
+++ src/dom.js
@@ -22,13 +22,40 @@ class ComponentWidget {
 
   init () {
     this.component = new this.constructor(this.properties, this.children)
+    if (this.properties && this.properties.ref && refsStack.length > 0) {
+      refsStack[refsStack.length - 1][this.properties.ref] = this.component
+    }
     return this.component.element
   }
 
   update (oldWidget, oldElement) {
+    let oldRef, newRef
+
+    if (oldWidget.properties && oldWidget.properties.ref) {
+      oldRef = oldWidget.properties.ref
+    }
+
+    if (this.properties && this.properties.ref) {
+      newRef = this.properties.ref
+    }
+
     if (this.constructor === oldWidget.constructor && typeof oldWidget.component.update === 'function') {
+      if (newRef !== oldRef && refsStack.length > 0) {
+        delete refsStack[refsStack.length - 1][oldRef]
+        refsStack[refsStack.length - 1][newRef] = oldWidget.component
+      }
+
       oldWidget.component.update(this.properties, this.children)
     } else {
+      if (refsStack.length > 0) {
+        if (newRef !== oldRef) {
+          delete refsStack[refsStack.length - 1][oldRef]
+        }
+        if (newRef) {
+          refsStack[refsStack.length - 1][newRef] = this.component
+        }
+      }
+
       let newElement = this.init()
       oldElement.parentNode.replaceChild(newElement, oldElement)
     }
--- test/unit/dom.test.js
+++ test/unit/dom.test.js
@@ -3,7 +3,6 @@
 import etch from '../../src/index'
 
 describe('etch.dom', () => {
-
   describe('when a component constructor is used as a tag name', () => {
     describe('on initial render', () => {
       it('constructs the component with the specified properties and children, then appends its element to the DOM', () => {
@@ -180,5 +179,73 @@ describe('etch.dom', () => {
         })
       })
     })
+
+    describe('when the child component constructor tag has a ref property', () => {
+      it('creates a reference to the child component object on the parent component', async () => {
+        class ChildComponentA {
+          constructor (properties) {
+            this.properties = properties
+            etch.createElement(this)
+          }
+
+          render () {
+            return <div ref='self'>A</div>
+          }
+
+          update (properties) {
+            this.properties = properties
+          }
+        }
+
+        class ChildComponentB {
+          constructor (properties) {
+            this.properties = properties
+            etch.createElement(this)
+          }
+
+          render () {
+            return <div ref='self'>B</div>
+          }
+        }
+
+        let parentComponent = {
+          condition: true,
+          refName: 'child',
+
+          render () {
+            if (this.condition) {
+              return <div><ChildComponentA ref={this.refName}></ChildComponentA></div>
+            } else {
+              return <div><ChildComponentB ref={this.refName}></ChildComponentB></div>
+            }
+          }
+        }
+
+        let element = etch.createElement(parentComponent)
+
+        expect(parentComponent.refs.child instanceof ChildComponentA).to.be.true
+        expect(parentComponent.refs.child.properties.ref).to.equal('child')
+        expect(parentComponent.refs.child.refs.self.textContent).to.equal('A')
+
+        parentComponent.refName = 'kid'
+        etch.updateElement(parentComponent)
+        await etch.getScheduler().getNextUpdatePromise()
+
+        expect(parentComponent.refs.child).to.be.undefined
+        expect(parentComponent.refs.kid instanceof ChildComponentA).to.be.true
+        expect(parentComponent.refs.kid.properties.ref).to.equal('kid')
+        expect(parentComponent.refs.kid.refs.self.textContent).to.equal('A')
+
+        parentComponent.refName = 'child'
+        parentComponent.condition = false
+        etch.updateElement(parentComponent)
+        await etch.getScheduler().getNextUpdatePromise()
+
+        expect(parentComponent.refs.kid).to.be.undefined
+        expect(parentComponent.refs.child instanceof ChildComponentB).to.be.true
+        expect(parentComponent.refs.child.properties.ref).to.equal('child')
+        expect(parentComponent.refs.child.refs.self.textContent).to.equal('B')
+      });
+    });
   })
 })
