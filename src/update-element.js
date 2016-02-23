d92dbec73d688d6001d6d755c0410b7700bdf704
Nathan Sobo <nathan@github.com>
2016-02-22 21:48:06 -0700

    Document updateElement

--- src/update-element.js
+++ src/update-element.js
@@ -2,6 +2,17 @@ import {getScheduler} from './scheduler-assignment'
 import performElementUpdate from './perform-element-update'
 import componentsWithPendingUpdates from './components-with-pending-updates'
 
+// This function receives a component that has already been associated with an
+// element via a previous call to `createElement` and updates this element by
+// calling `render` on the component.
+//
+// It uses the scheduler to defer this update until the next animation frame,
+// and will only perform one update of a given component in a given frame. This
+// means you can call `updateElement` repeatedly in a given tick without causing
+// redundant updates.
+//
+// Returns a promise that will resolve when the requested update has been
+// completed.
 export default function updateElement (component) {
   let scheduler = getScheduler()
 
