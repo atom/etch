a76b8a0e2d41771668003f65bb5d854f22d29a6a
Michelle Tilley <binarymuse@github.com>
2016-04-14 16:38:45 -0700

    Don't export render

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import dom from './dom'
-import {render, initialize, update, updateSync, destroy, destroySync} from './component-helpers'
+import {initialize, update, updateSync, destroy, destroySync} from './component-helpers'
 import {setScheduler, getScheduler} from './scheduler-assignment'
 
 let etch = {
   dom,
-  render, initialize, update, updateSync, destroy, destroySync,
+  initialize, update, updateSync, destroy, destroySync,
   setScheduler, getScheduler
 }
 
