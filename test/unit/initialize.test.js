637e195578d73671041be9ffc8090f06d34db5ea
Adam Laycock <adam@arcath.net>
2017-04-03 20:06:13 +0100

    support multi dimensional refs

--- test/unit/initialize.test.js
+++ test/unit/initialize.test.js
@@ -65,4 +65,60 @@ describe('etch.initialize(component)', () => {
       etch.initialize(component)
     }).to.throw(/invalid falsy value/)
   })
+
+  it('allows for multi dimensional refs', async function () {
+    let component = {
+      render () {
+        return <div ref='divs[first]'><div ref='divs[second]'>second</div></div>
+      },
+
+      update () {}
+    }
+
+    etch.initialize(component)
+
+    expect('divs[first]' in component.refs).to.be.false
+    expect('divs' in component.refs).to.be.true
+    expect(component.refs.divs).to.have.all.keys(['first', 'second'])
+    expect(component.refs.divs.second.textContent).to.equal('second')
+
+  })
+
+  it('allows for deep multi dimensional refs', async function () {
+    let component = {
+      render () {
+        return <div ref='divs[test[first]]'>hello</div>
+      },
+
+      update () {}
+    }
+
+    etch.initialize(component)
+
+    expect(component.refs.divs.test.first.textContent).to.equal('hello')
+  })
+
+  it('allows updating multi dimensional refs', async function () {
+    let component = {
+      render () {
+        return (<div>
+          <div ref={'tests[num_' + testNumber + ']'}>Testing</div>
+        </div>)
+      },
+
+      update () {}
+    }
+
+    let testNumber = 0
+
+    etch.initialize(component)
+
+    expect(component.refs.tests.num_0.textContent).to.equal('Testing')
+
+    testNumber = 1
+
+    await etch.update(component)
+
+    expect(component.refs.tests.num_1.textContent).to.equal('Testing')
+  })
 })
