2d1c76e9891894f30059d6b344dad485395ffd16
aminya <aminyahyaabadi74@gmail.com>
2020-03-12 23:27:52 -0500

    index using export and export default
    instead of module.exports in index
    
    import dom instead
    
    Update index.ts

--- lib_src/index.ts
+++ lib_src/index.ts
@@ -1,9 +1,16 @@
-const dom = require('./dom')
+import {dom} from './dom'
 import {render} from "./render";
 import {initialize, update, updateSync, destroy, destroySync} from './component-helpers'
 import {setScheduler, getScheduler} from './scheduler-assignment'
 
 module.exports = {
+export {
+  dom, render,
+  initialize, update, updateSync, destroy, destroySync,
+  setScheduler, getScheduler
+}
+
+export default {
   dom, render,
   initialize, update, updateSync, destroy, destroySync,
   setScheduler, getScheduler
