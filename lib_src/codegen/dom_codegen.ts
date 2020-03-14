dffd2fd230a06099efee871ad335b0f6e0713a96
aminya <aminyahyaabadi74@gmail.com>
2020-03-13 21:01:40 -0500

    JSX support in dom_codegen.
    move to folder codegen

--- lib_src/codegen/dom_codegen.ts
+++ lib_src/codegen/dom_codegen.ts
@@ -1,19 +1,36 @@
 const fs =  require('fs')
-import {SVG_TAGS} from './svg-tags'
-import {HTML_TAGS} from './html-tags'
+import {SVG_TAGS} from '../svg-tags'
+import {HTML_TAGS} from '../html-tags'
 
-function dom_codegen(){
-    let file = "dom_codegen.txt"
+function EtchDOM_codegen(){
+    let file = "EtchDOM_codegen.txt"
     fs.open(file, "w", (err: Error, fd :number) => {
         if (err) throw err;
         for (const tag of HTML_TAGS) {
-            fs.appendFile(file,  `  ${tag}: EtchCreateElement<"${tag}", any>\n`, (err :Error) => {if (err) throw err;})
+            fs.appendFile(file,  `  ${tag}: EtchCreateElement<"${tag}", JSX.IntrinsicElements["${tag}"]>\n`, (err :Error) => {if (err) throw err;})
         }
         for (const tag of SVG_TAGS) {
-            fs.appendFile(file,  `  ${tag}: EtchCreateElement<"${tag}", any>\n`, (err :Error) => {if (err) throw err;})
+            fs.appendFile(file,  `  ${tag}: EtchCreateElement<"${tag}", JSX.IntrinsicElements["${tag}"]>\n`, (err :Error) => {if (err) throw err;})
         }
         fs.close(fd, (err :Error) => {if (err) throw err;})
     })
 }
 
-dom_codegen()
+EtchDOM_codegen()
+
+
+function JSX_codegen(){
+    let file = "JSX_codegen.txt"
+    fs.open(file, "w", (err: Error, fd :number) => {
+        if (err) throw err;
+        for (const tag of HTML_TAGS) {
+            fs.appendFile(file,`      ${tag}: any\n`, (err :Error) => {if (err) throw err;})
+        }
+        for (const tag of SVG_TAGS) {
+            fs.appendFile(file,  `      ${tag}: any\n`, (err :Error) => {if (err) throw err;})
+        }
+        fs.close(fd, (err :Error) => {if (err) throw err;})
+    })
+}
+
+JSX_codegen()
