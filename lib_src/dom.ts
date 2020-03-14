6eb6548ad3cc3d448067c441adb9a57fddba5322
aminya <aminyahyaabadi74@gmail.com>
2020-03-13 21:01:51 -0500

    JSX support in dom Fix bug in dom ( extends string for tag)

--- lib_src/dom.ts
+++ lib_src/dom.ts
@@ -11,142 +11,282 @@ export interface EtchElement<T extends string, P = any> {
 
 export type EtchCreateElement<T extends string, P> = (props :P, ...children :Array<string | number | object>) => EtchElement<T, P>
 
+declare global {
+  export namespace JSX {
+    export interface Element extends EtchElement<any, any> {}
+    export interface IntrinsicElements {
+
+      // constraint on props of each element
+      // auto-generated using dom_codegen.ts:
+      a: any
+      abbr: any
+      address: any
+      aside: any
+      article: any
+      audio: any
+      b: any
+      bdi: any
+      bdo: any
+      blockquote: any
+      body: any
+      button: any
+      caption: any
+      canvas: any
+      code: any
+      cite: any
+      colgroup: any
+      datalist: any
+      del: any
+      dd: any
+      details: any
+      dialog: any
+      div: any
+      dfn: any
+      dt: any
+      dl: any
+      em: any
+      fieldset: any
+      figcaption: any
+      figure: any
+      footer: any
+      form: any
+      h1: any
+      h3: any
+      h4: any
+      h2: any
+      h5: any
+      h6: any
+      header: any
+      head: any
+      html: any
+      i: any
+      iframe: any
+      kbd: any
+      label: any
+      ins: any
+      legend: any
+      main: any
+      li: any
+      mark: any
+      meter: any
+      map: any
+      menu: any
+      object: any
+      nav: any
+      ol: any
+      noscript: any
+      optgroup: any
+      option: any
+      pre: any
+      output: any
+      p: any
+      q: any
+      progress: any
+      ruby: any
+      s: any
+      rt: any
+      samp: any
+      script: any
+      section: any
+      small: any
+      select: any
+      strong: any
+      style: any
+      rp: any
+      summary: any
+      sup: any
+      table: any
+      sub: any
+      tbody: any
+      textarea: any
+      span: any
+      td: any
+      tfoot: any
+      title: any
+      time: any
+      th: any
+      thead: any
+      u: any
+      tr: any
+      ul: any
+      var: any
+      video: any
+      area: any
+      br: any
+      base: any
+      command: any
+      img: any
+      hr: any
+      col: any
+      input: any
+      embed: any
+      keygen: any
+      link: any
+      meta: any
+      source: any
+      wbr: any
+      circle: any
+      clipPath: any
+      param: any
+      defs: any
+      ellipse: any
+      g: any
+      line: any
+      image: any
+      linearGradient: any
+      mask: any
+      path: any
+      track: any
+      pattern: any
+      polygon: any
+      polyline: any
+      rect: any
+      radialGradient: any
+      svg: any
+      text: any
+      tspan: any
+      stop: any
+    }
+  }
+}
+
 export interface EtchDOM {
   <T extends string, P>(tag :T, props :P, ...children :Array<string | number | object>) :EtchElement<T, P>
 
   // auto-generated using dom_codegen.ts:
-  a: EtchCreateElement<"a", any>
-  abbr: EtchCreateElement<"abbr", any>
-  article: EtchCreateElement<"article", any>
-  address: EtchCreateElement<"address", any>
-  aside: EtchCreateElement<"aside", any>
-  audio: EtchCreateElement<"audio", any>
-  b: EtchCreateElement<"b", any>
-  bdi: EtchCreateElement<"bdi", any>
-  bdo: EtchCreateElement<"bdo", any>
-  blockquote: EtchCreateElement<"blockquote", any>
-  body: EtchCreateElement<"body", any>
-  button: EtchCreateElement<"button", any>
-  canvas: EtchCreateElement<"canvas", any>
-  caption: EtchCreateElement<"caption", any>
-  cite: EtchCreateElement<"cite", any>
-  code: EtchCreateElement<"code", any>
-  colgroup: EtchCreateElement<"colgroup", any>
-  datalist: EtchCreateElement<"datalist", any>
-  dd: EtchCreateElement<"dd", any>
-  del: EtchCreateElement<"del", any>
-  details: EtchCreateElement<"details", any>
-  dfn: EtchCreateElement<"dfn", any>
-  dialog: EtchCreateElement<"dialog", any>
-  div: EtchCreateElement<"div", any>
-  dl: EtchCreateElement<"dl", any>
-  dt: EtchCreateElement<"dt", any>
-  em: EtchCreateElement<"em", any>
-  fieldset: EtchCreateElement<"fieldset", any>
-  figcaption: EtchCreateElement<"figcaption", any>
-  figure: EtchCreateElement<"figure", any>
-  footer: EtchCreateElement<"footer", any>
-  form: EtchCreateElement<"form", any>
-  h1: EtchCreateElement<"h1", any>
-  h2: EtchCreateElement<"h2", any>
-  h3: EtchCreateElement<"h3", any>
-  h4: EtchCreateElement<"h4", any>
-  h5: EtchCreateElement<"h5", any>
-  h6: EtchCreateElement<"h6", any>
-  head: EtchCreateElement<"head", any>
-  header: EtchCreateElement<"header", any>
-  html: EtchCreateElement<"html", any>
-  i: EtchCreateElement<"i", any>
-  ins: EtchCreateElement<"ins", any>
-  iframe: EtchCreateElement<"iframe", any>
-  kbd: EtchCreateElement<"kbd", any>
-  label: EtchCreateElement<"label", any>
-  legend: EtchCreateElement<"legend", any>
-  li: EtchCreateElement<"li", any>
-  main: EtchCreateElement<"main", any>
-  map: EtchCreateElement<"map", any>
-  mark: EtchCreateElement<"mark", any>
-  menu: EtchCreateElement<"menu", any>
-  meter: EtchCreateElement<"meter", any>
-  nav: EtchCreateElement<"nav", any>
-  noscript: EtchCreateElement<"noscript", any>
-  object: EtchCreateElement<"object", any>
-  ol: EtchCreateElement<"ol", any>
-  optgroup: EtchCreateElement<"optgroup", any>
-  option: EtchCreateElement<"option", any>
-  output: EtchCreateElement<"output", any>
-  p: EtchCreateElement<"p", any>
-  pre: EtchCreateElement<"pre", any>
-  progress: EtchCreateElement<"progress", any>
-  q: EtchCreateElement<"q", any>
-  rp: EtchCreateElement<"rp", any>
-  rt: EtchCreateElement<"rt", any>
-  ruby: EtchCreateElement<"ruby", any>
-  s: EtchCreateElement<"s", any>
-  samp: EtchCreateElement<"samp", any>
-  script: EtchCreateElement<"script", any>
-  section: EtchCreateElement<"section", any>
-  select: EtchCreateElement<"select", any>
-  small: EtchCreateElement<"small", any>
-  span: EtchCreateElement<"span", any>
-  strong: EtchCreateElement<"strong", any>
-  sub: EtchCreateElement<"sub", any>
-  style: EtchCreateElement<"style", any>
-  summary: EtchCreateElement<"summary", any>
-  sup: EtchCreateElement<"sup", any>
-  table: EtchCreateElement<"table", any>
-  tbody: EtchCreateElement<"tbody", any>
-  td: EtchCreateElement<"td", any>
-  textarea: EtchCreateElement<"textarea", any>
-  tfoot: EtchCreateElement<"tfoot", any>
-  th: EtchCreateElement<"th", any>
-  thead: EtchCreateElement<"thead", any>
-  time: EtchCreateElement<"time", any>
-  title: EtchCreateElement<"title", any>
-  tr: EtchCreateElement<"tr", any>
-  u: EtchCreateElement<"u", any>
-  ul: EtchCreateElement<"ul", any>
-  var: EtchCreateElement<"var", any>
-  video: EtchCreateElement<"video", any>
-  area: EtchCreateElement<"area", any>
-  base: EtchCreateElement<"base", any>
-  br: EtchCreateElement<"br", any>
-  col: EtchCreateElement<"col", any>
-  command: EtchCreateElement<"command", any>
-  embed: EtchCreateElement<"embed", any>
-  hr: EtchCreateElement<"hr", any>
-  img: EtchCreateElement<"img", any>
-  input: EtchCreateElement<"input", any>
-  keygen: EtchCreateElement<"keygen", any>
-  link: EtchCreateElement<"link", any>
-  meta: EtchCreateElement<"meta", any>
-  param: EtchCreateElement<"param", any>
-  source: EtchCreateElement<"source", any>
-  track: EtchCreateElement<"track", any>
-  wbr: EtchCreateElement<"wbr", any>
-  circle: EtchCreateElement<"circle", any>
-  clipPath: EtchCreateElement<"clipPath", any>
-  defs: EtchCreateElement<"defs", any>
-  ellipse: EtchCreateElement<"ellipse", any>
-  g: EtchCreateElement<"g", any>
-  image: EtchCreateElement<"image", any>
-  line: EtchCreateElement<"line", any>
-  linearGradient: EtchCreateElement<"linearGradient", any>
-  mask: EtchCreateElement<"mask", any>
-  path: EtchCreateElement<"path", any>
-  pattern: EtchCreateElement<"pattern", any>
-  polygon: EtchCreateElement<"polygon", any>
-  polyline: EtchCreateElement<"polyline", any>
-  radialGradient: EtchCreateElement<"radialGradient", any>
-  rect: EtchCreateElement<"rect", any>
-  stop: EtchCreateElement<"stop", any>
-  svg: EtchCreateElement<"svg", any>
-  text: EtchCreateElement<"text", any>
-  tspan: EtchCreateElement<"tspan", any>
+  address: EtchCreateElement<"address", JSX.IntrinsicElements["address"]>
+  a: EtchCreateElement<"a", JSX.IntrinsicElements["a"]>
+  abbr: EtchCreateElement<"abbr", JSX.IntrinsicElements["abbr"]>
+  article: EtchCreateElement<"article", JSX.IntrinsicElements["article"]>
+  aside: EtchCreateElement<"aside", JSX.IntrinsicElements["aside"]>
+  audio: EtchCreateElement<"audio", JSX.IntrinsicElements["audio"]>
+  b: EtchCreateElement<"b", JSX.IntrinsicElements["b"]>
+  bdi: EtchCreateElement<"bdi", JSX.IntrinsicElements["bdi"]>
+  blockquote: EtchCreateElement<"blockquote", JSX.IntrinsicElements["blockquote"]>
+  body: EtchCreateElement<"body", JSX.IntrinsicElements["body"]>
+  bdo: EtchCreateElement<"bdo", JSX.IntrinsicElements["bdo"]>
+  button: EtchCreateElement<"button", JSX.IntrinsicElements["button"]>
+  caption: EtchCreateElement<"caption", JSX.IntrinsicElements["caption"]>
+  canvas: EtchCreateElement<"canvas", JSX.IntrinsicElements["canvas"]>
+  cite: EtchCreateElement<"cite", JSX.IntrinsicElements["cite"]>
+  code: EtchCreateElement<"code", JSX.IntrinsicElements["code"]>
+  colgroup: EtchCreateElement<"colgroup", JSX.IntrinsicElements["colgroup"]>
+  datalist: EtchCreateElement<"datalist", JSX.IntrinsicElements["datalist"]>
+  dd: EtchCreateElement<"dd", JSX.IntrinsicElements["dd"]>
+  del: EtchCreateElement<"del", JSX.IntrinsicElements["del"]>
+  details: EtchCreateElement<"details", JSX.IntrinsicElements["details"]>
+  dialog: EtchCreateElement<"dialog", JSX.IntrinsicElements["dialog"]>
+  dfn: EtchCreateElement<"dfn", JSX.IntrinsicElements["dfn"]>
+  div: EtchCreateElement<"div", JSX.IntrinsicElements["div"]>
+  dl: EtchCreateElement<"dl", JSX.IntrinsicElements["dl"]>
+  dt: EtchCreateElement<"dt", JSX.IntrinsicElements["dt"]>
+  em: EtchCreateElement<"em", JSX.IntrinsicElements["em"]>
+  figcaption: EtchCreateElement<"figcaption", JSX.IntrinsicElements["figcaption"]>
+  fieldset: EtchCreateElement<"fieldset", JSX.IntrinsicElements["fieldset"]>
+  figure: EtchCreateElement<"figure", JSX.IntrinsicElements["figure"]>
+  form: EtchCreateElement<"form", JSX.IntrinsicElements["form"]>
+  footer: EtchCreateElement<"footer", JSX.IntrinsicElements["footer"]>
+  h1: EtchCreateElement<"h1", JSX.IntrinsicElements["h1"]>
+  h2: EtchCreateElement<"h2", JSX.IntrinsicElements["h2"]>
+  h3: EtchCreateElement<"h3", JSX.IntrinsicElements["h3"]>
+  h4: EtchCreateElement<"h4", JSX.IntrinsicElements["h4"]>
+  h5: EtchCreateElement<"h5", JSX.IntrinsicElements["h5"]>
+  h6: EtchCreateElement<"h6", JSX.IntrinsicElements["h6"]>
+  head: EtchCreateElement<"head", JSX.IntrinsicElements["head"]>
+  header: EtchCreateElement<"header", JSX.IntrinsicElements["header"]>
+  html: EtchCreateElement<"html", JSX.IntrinsicElements["html"]>
+  i: EtchCreateElement<"i", JSX.IntrinsicElements["i"]>
+  iframe: EtchCreateElement<"iframe", JSX.IntrinsicElements["iframe"]>
+  ins: EtchCreateElement<"ins", JSX.IntrinsicElements["ins"]>
+  kbd: EtchCreateElement<"kbd", JSX.IntrinsicElements["kbd"]>
+  label: EtchCreateElement<"label", JSX.IntrinsicElements["label"]>
+  legend: EtchCreateElement<"legend", JSX.IntrinsicElements["legend"]>
+  main: EtchCreateElement<"main", JSX.IntrinsicElements["main"]>
+  li: EtchCreateElement<"li", JSX.IntrinsicElements["li"]>
+  map: EtchCreateElement<"map", JSX.IntrinsicElements["map"]>
+  mark: EtchCreateElement<"mark", JSX.IntrinsicElements["mark"]>
+  menu: EtchCreateElement<"menu", JSX.IntrinsicElements["menu"]>
+  meter: EtchCreateElement<"meter", JSX.IntrinsicElements["meter"]>
+  nav: EtchCreateElement<"nav", JSX.IntrinsicElements["nav"]>
+  noscript: EtchCreateElement<"noscript", JSX.IntrinsicElements["noscript"]>
+  object: EtchCreateElement<"object", JSX.IntrinsicElements["object"]>
+  ol: EtchCreateElement<"ol", JSX.IntrinsicElements["ol"]>
+  optgroup: EtchCreateElement<"optgroup", JSX.IntrinsicElements["optgroup"]>
+  option: EtchCreateElement<"option", JSX.IntrinsicElements["option"]>
+  output: EtchCreateElement<"output", JSX.IntrinsicElements["output"]>
+  pre: EtchCreateElement<"pre", JSX.IntrinsicElements["pre"]>
+  p: EtchCreateElement<"p", JSX.IntrinsicElements["p"]>
+  progress: EtchCreateElement<"progress", JSX.IntrinsicElements["progress"]>
+  q: EtchCreateElement<"q", JSX.IntrinsicElements["q"]>
+  rp: EtchCreateElement<"rp", JSX.IntrinsicElements["rp"]>
+  rt: EtchCreateElement<"rt", JSX.IntrinsicElements["rt"]>
+  ruby: EtchCreateElement<"ruby", JSX.IntrinsicElements["ruby"]>
+  s: EtchCreateElement<"s", JSX.IntrinsicElements["s"]>
+  samp: EtchCreateElement<"samp", JSX.IntrinsicElements["samp"]>
+  script: EtchCreateElement<"script", JSX.IntrinsicElements["script"]>
+  section: EtchCreateElement<"section", JSX.IntrinsicElements["section"]>
+  select: EtchCreateElement<"select", JSX.IntrinsicElements["select"]>
+  small: EtchCreateElement<"small", JSX.IntrinsicElements["small"]>
+  strong: EtchCreateElement<"strong", JSX.IntrinsicElements["strong"]>
+  span: EtchCreateElement<"span", JSX.IntrinsicElements["span"]>
+  style: EtchCreateElement<"style", JSX.IntrinsicElements["style"]>
+  sub: EtchCreateElement<"sub", JSX.IntrinsicElements["sub"]>
+  summary: EtchCreateElement<"summary", JSX.IntrinsicElements["summary"]>
+  sup: EtchCreateElement<"sup", JSX.IntrinsicElements["sup"]>
+  table: EtchCreateElement<"table", JSX.IntrinsicElements["table"]>
+  tbody: EtchCreateElement<"tbody", JSX.IntrinsicElements["tbody"]>
+  td: EtchCreateElement<"td", JSX.IntrinsicElements["td"]>
+  textarea: EtchCreateElement<"textarea", JSX.IntrinsicElements["textarea"]>
+  tfoot: EtchCreateElement<"tfoot", JSX.IntrinsicElements["tfoot"]>
+  th: EtchCreateElement<"th", JSX.IntrinsicElements["th"]>
+  thead: EtchCreateElement<"thead", JSX.IntrinsicElements["thead"]>
+  time: EtchCreateElement<"time", JSX.IntrinsicElements["time"]>
+  title: EtchCreateElement<"title", JSX.IntrinsicElements["title"]>
+  tr: EtchCreateElement<"tr", JSX.IntrinsicElements["tr"]>
+  ul: EtchCreateElement<"ul", JSX.IntrinsicElements["ul"]>
+  u: EtchCreateElement<"u", JSX.IntrinsicElements["u"]>
+  var: EtchCreateElement<"var", JSX.IntrinsicElements["var"]>
+  video: EtchCreateElement<"video", JSX.IntrinsicElements["video"]>
+  area: EtchCreateElement<"area", JSX.IntrinsicElements["area"]>
+  base: EtchCreateElement<"base", JSX.IntrinsicElements["base"]>
+  br: EtchCreateElement<"br", JSX.IntrinsicElements["br"]>
+  col: EtchCreateElement<"col", JSX.IntrinsicElements["col"]>
+  command: EtchCreateElement<"command", JSX.IntrinsicElements["command"]>
+  embed: EtchCreateElement<"embed", JSX.IntrinsicElements["embed"]>
+  hr: EtchCreateElement<"hr", JSX.IntrinsicElements["hr"]>
+  img: EtchCreateElement<"img", JSX.IntrinsicElements["img"]>
+  input: EtchCreateElement<"input", JSX.IntrinsicElements["input"]>
+  keygen: EtchCreateElement<"keygen", JSX.IntrinsicElements["keygen"]>
+  link: EtchCreateElement<"link", JSX.IntrinsicElements["link"]>
+  meta: EtchCreateElement<"meta", JSX.IntrinsicElements["meta"]>
+  param: EtchCreateElement<"param", JSX.IntrinsicElements["param"]>
+  source: EtchCreateElement<"source", JSX.IntrinsicElements["source"]>
+  track: EtchCreateElement<"track", JSX.IntrinsicElements["track"]>
+  wbr: EtchCreateElement<"wbr", JSX.IntrinsicElements["wbr"]>
+  circle: EtchCreateElement<"circle", JSX.IntrinsicElements["circle"]>
+  clipPath: EtchCreateElement<"clipPath", JSX.IntrinsicElements["clipPath"]>
+  defs: EtchCreateElement<"defs", JSX.IntrinsicElements["defs"]>
+  ellipse: EtchCreateElement<"ellipse", JSX.IntrinsicElements["ellipse"]>
+  g: EtchCreateElement<"g", JSX.IntrinsicElements["g"]>
+  image: EtchCreateElement<"image", JSX.IntrinsicElements["image"]>
+  line: EtchCreateElement<"line", JSX.IntrinsicElements["line"]>
+  linearGradient: EtchCreateElement<"linearGradient", JSX.IntrinsicElements["linearGradient"]>
+  mask: EtchCreateElement<"mask", JSX.IntrinsicElements["mask"]>
+  path: EtchCreateElement<"path", JSX.IntrinsicElements["path"]>
+  pattern: EtchCreateElement<"pattern", JSX.IntrinsicElements["pattern"]>
+  polygon: EtchCreateElement<"polygon", JSX.IntrinsicElements["polygon"]>
+  polyline: EtchCreateElement<"polyline", JSX.IntrinsicElements["polyline"]>
+  radialGradient: EtchCreateElement<"radialGradient", JSX.IntrinsicElements["radialGradient"]>
+  rect: EtchCreateElement<"rect", JSX.IntrinsicElements["rect"]>
+  stop: EtchCreateElement<"stop", JSX.IntrinsicElements["stop"]>
+  svg: EtchCreateElement<"svg", JSX.IntrinsicElements["svg"]>
+  text: EtchCreateElement<"text", JSX.IntrinsicElements["text"]>
+  tspan: EtchCreateElement<"tspan", JSX.IntrinsicElements["tspan"]>
 }
 
 
-export function dom<T extends string, P>(tag :T extends string, props :P, ...children :Array<string | number | object>) :EtchElement<T, P> {
+
+export function dom<T extends string, P>(tag :T, props :P, ...children :Array<string | number | object>) :EtchElement<T, P> {
 
   let ambiguous :Array<object> = []
 
@@ -200,13 +340,13 @@ export function dom<T extends string, P>(tag :T extends string, props :P, ...chi
 export declare const dom :EtchDOM
 
 for (const tagName of HTML_TAGS) {
-  dom[tagName] = function <tagName, P>(props :P, ...children :Array<string | number | object>) :EtchElement<tagName, P> {
+  dom[tagName] = function <tagName extends string, P>(props :P, ...children :Array<string | number | object>) :EtchElement<tagName, P> {
     return dom(tagName, props, ...children)
   }
 }
 
 for (const tagName of SVG_TAGS) {
-  dom[tagName] = function <tagName, P>(props :P, ...children :Array<string | number | object>) :EtchElement<tagName, P> {
+  dom[tagName] = function <tagName extends string, P>(props :P, ...children :Array<string | number | object>) :EtchElement<tagName, P> {
     return dom(tagName, props, ...children)
   }
 }
