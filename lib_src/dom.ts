ecf3e291dfacf7ef21c05f7fa75959d8a3606ad2
aminya <aminyahyaabadi74@gmail.com>
2020-03-15 17:55:56 -0500

    Separate HTML File

--- lib_src/dom.ts
+++ lib_src/dom.ts
@@ -1,5 +1,6 @@
 const EVENT_LISTENER_PROPS = require('./event-listener-props')
 const SVG_TAGS = require('./svg-tags')
+import {HTML_TAGS} from './html-tags'
 
 function dom (tag, props, ...children) {
   let ambiguous = []
@@ -51,21 +52,6 @@ function dom (tag, props, ...children) {
   return {tag, props, children, ambiguous}
 }
 
-const HTML_TAGS = [
-  'a', 'abbr', 'address', 'article', 'aside', 'audio', 'b', 'bdi', 'bdo',
-  'blockquote', 'body', 'button', 'canvas', 'caption', 'cite', 'code',
-  'colgroup', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'div', 'dl',
-  'dt', 'em', 'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2',
-  'h3', 'h4', 'h5', 'h6', 'head', 'header', 'html', 'i', 'iframe', 'ins', 'kbd',
-  'label', 'legend', 'li', 'main', 'map', 'mark', 'menu', 'meter', 'nav',
-  'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'pre',
-  'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section',
-  'select', 'small', 'span', 'strong', 'style', 'sub', 'summary', 'sup',
-  'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title',
-  'tr', 'u', 'ul', 'var', 'video', 'area', 'base', 'br', 'col', 'command',
-  'embed', 'hr', 'img', 'input', 'keygen', 'link', 'meta', 'param', 'source',
-  'track', 'wbr'
-]
 
 for (const tagName of HTML_TAGS) {
   dom[tagName] = (props, ...children) => {
