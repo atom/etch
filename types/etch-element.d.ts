35623805d348d8a5ace82b43f7b81a88ef832b4d
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-27 07:50:42 -0500

    Etch element extra props

--- types/etch-element.d.ts
+++ types/etch-element.d.ts
@@ -1,3 +1,18 @@
 /** Etch class props */
 type Props = object
 
+/** Etch element extra props */
+interface EtchExtraProps {
+  ref?: string
+  className?: string
+  on?: {
+    click?: (e: MouseEvent) => any
+    [name: string]: ((e: any) => any) | undefined
+  }
+  dataset?: {
+    [propName: string]: string | number | null
+  }
+  innerHTML?: string
+  innerText?: string
+  key?: any
+}
